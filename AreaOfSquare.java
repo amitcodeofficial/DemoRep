4bc0c03fb2015777e5569cc8f182ee123da0088b
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-04-14 18:03:09 +0530

    New Branch UC1\

---
+++ AreaOfSquare.java
@@ -0,0 +1,12 @@
+package com.bridgelab.git;
+
+public class AreaOfSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int side=4;
+		int area=side*side;
+		System.out.println("Area of trianlge="+area);
+	}
+
+}
